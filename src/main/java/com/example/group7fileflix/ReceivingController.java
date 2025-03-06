b632eb8639227cdc9a91eabb73b6208a190c20b4
AnshDhunna <adhunna1172@conestogac.on.ca>
2025-03-06 14:34:45 -0500

    "GUI pages"

---
+++ src/main/java/com/example/group7fileflix/ReceivingController.java
@@ -0,0 +1,18 @@
+package com.example.group7fileflix;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class ReceivingController extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
